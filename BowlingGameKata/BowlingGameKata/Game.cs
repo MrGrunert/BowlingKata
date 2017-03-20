981b52d176f015fcbdcdaf8e13e8ff14590c510c
Sinnlos <jimmy.grunert@gmail.com>
2017-03-20 23:31:21 +0100

    test commit

---
+++ BowlingGameKata/BowlingGameKata/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGameKata
+{
+    public class Game
+    {
+        private int score = 0;
+
+        public void roll(int pins)
+        {
+            score += pins;
+        }
+
+        public int scoreGame()
+        {
+            return score;
+        }
+
+       //add this to test gitpus
+       // test igen då..
+
+    }
+}
---
+++ BowlingGameKata/BowlingKataUnitTests/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingGameKata;
+
+namespace BowlingKataUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Game g;
+
+        public UnitTest1()
+        {
+            g = new Game();
+        }
+           
+
+        [TestMethod]
+        public void DoesGameExist()
+        {
+            //arrange
+            
+
+            //act
+           
+
+            //assert
+            Assert.IsNotNull(g);
+        }
+
+        [TestMethod]
+        public void GutterGameReturns0()
+        {
+            //arrange
+
+            int pins = 0;
+
+            //act
+           
+
+
+            //assert
+            Assert.AreEqual(0, g.scoreGame());
+        }
+
+        [TestMethod]
+        public void singlePinGameReturns20()
+        {
+            //arrange
+
+            //act
+            rollMany(20, 1);
+           
+
+
+            //assert
+            Assert.AreEqual(20, g.scoreGame());
+        }
+
+
+        public void rollMany(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+
+            {
+                g.roll(pins);
+            }
+        }
+
+
+    }
+}
